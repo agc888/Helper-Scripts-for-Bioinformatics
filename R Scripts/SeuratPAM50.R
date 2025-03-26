62e009654bdf453f46856e493c139f44abbe2e2d
agc888 <88018986+agc888@users.noreply.github.com>
2025-03-26 15:27:31 +1000

    Update SeuratPAM50.R

--- R Scripts/SeuratPAM50.R
+++ R Scripts/SeuratPAM50.R
@@ -331,11 +331,10 @@ EstimatePAM50 <- function(seurat_obj, group.by, n = 3, assay = DefaultAssay(seur
     
     verbose_message(message_text = "Adding results to Seurat Object", verbose = verbose)
 
+    return(df)
+    
     # Reorder df to match the rownames of the Seurat object metadata
     df <- df[rownames(seurat_obj@meta.data), , drop = FALSE]
-
-    return(df)
-    # Add the data frame as metadata
     seurat_obj <- AddMetaData(seurat_obj, metadata = df)
 
     return(seurat_obj)
