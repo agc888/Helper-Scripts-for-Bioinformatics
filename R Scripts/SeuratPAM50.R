3a1dd30d49520b2f00fffd08d270b296146cbca6
agc888 <88018986+agc888@users.noreply.github.com>
2025-03-26 13:17:47 +1000

    Update SeuratPAM50.R

--- R Scripts/SeuratPAM50.R
+++ R Scripts/SeuratPAM50.R
@@ -1,6 +1,5 @@
 library(ctc)
 library(heatmap.plus)
-library(here)
 library(Seurat)
 library(ggplot2)
 library(dplyr)
@@ -83,11 +82,11 @@ EstimatePAM50 <- function(seurat_obj, group.by, n = 3, assay = DefaultAssay(seur
     #7. Calculate Spearman's rank correlation between each sample and each subtype centroid (in pam50_centroids.txt)
     #8. Assign the class of the most highly correlated centroid to each sample
     
-    calibrationFile<- here("https://raw.githubusercontent.com/agc888/Helper-Scripts-for-Bioinformatics/refs/heads/main/R%20Scripts/PAM50_R/mediansPerDataset_v2.txt")
+    calibrationFile<- "https://raw.githubusercontent.com/agc888/Helper-Scripts-for-Bioinformatics/refs/heads/main/R%20Scripts/PAM50_R/mediansPerDataset_v2.txt"
     
 
-    trainCentroids<- here("https://raw.githubusercontent.com/agc888/Helper-Scripts-for-Bioinformatics/refs/heads/main/R%20Scripts/PAM50_R/pam50_centroids.txt")
-    trainFile<- here("https://raw.githubusercontent.com/agc888/Helper-Scripts-for-Bioinformatics/refs/heads/main/R%20Scripts/PAM50_R/220arrays_nonUBCcommon+12normal_50g.txt")
+    trainCentroids<- "https://raw.githubusercontent.com/agc888/Helper-Scripts-for-Bioinformatics/refs/heads/main/R%20Scripts/PAM50_R/pam50_centroids.txt"
+    trainFile<- "https://raw.githubusercontent.com/agc888/Helper-Scripts-for-Bioinformatics/refs/heads/main/R%20Scripts/PAM50_R/220arrays_nonUBCcommon+12normal_50g.txt"
     proliferationGenes<-c("CCNB1","UBE2C","BIRC5","KNTC2","CDC20","PTTG1","RRM2","MKI67","TYMS","CEP55","CDCA1")
     stdArray<-stdArray # just for visualization, and only set to F if many missing genes
     
